866d299ff7d5f3c6de7c990d78fc4eb149d00db9
Luketruke <lucaszabalett@gmail.com>
2022-06-20 02:55:10 -0300

    cosas varias

---
+++ Formularios/Productos/Productos.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="C#" MasterPageFile="../../Site.Master" AutoEventWireup="true" CodeBehind="Productos.aspx.cs" Inherits="Proyecto_Final_LAB.Formularios.Productos.Productos" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2><%: Title %>Productos</h2>
+
+    <asp:GridView ID="dgvProductos" runat="server" CssClass="table"></asp:GridView>
+
+</asp:Content>
---
+++ Formularios/Productos/Productos.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using negocios;
+
+namespace Proyecto_Final_LAB.Formularios.Productos
+{
+    public partial class Productos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProductoNegocio pn = new ProductoNegocio();
+            dgvProductos.DataSource = pn.listarProductos();
+            dgvProductos.DataBind();
+        }
+    }
+}

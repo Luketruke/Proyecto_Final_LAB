6d9d2612eb90c34bc59e4e48cf16e5b9ff2a9c65
Luketruke <lucaszabalett@gmail.com>
2022-07-07 01:21:27 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/Luketruke/Proyecto_Final_LAB""
    This reverts commit 65ee627b77641454367878feb82cb6991a353386.

--- Formularios/Proveedores/ProveedoresABM.aspx
+++ Formularios/Proveedores/ProveedoresABM.aspx
@@ -5,7 +5,7 @@
     <br />
 
     <div class="mb-3">
-        <label for="txtCod" class="form-label">Codigo</label>       
+        <label for="txtCodigo" class="form-label">Codigo</label>       
         <asp:TextBox runat="server" ID="txtCodigo" class="form-control" />
         <asp:RequiredFieldValidator style="color:red; font-size: 15px" runat="server" ControlToValidate="txtCodigo" ErrorMessage="*"></asp:RequiredFieldValidator>
     </div>
@@ -15,30 +15,30 @@
         <asp:RequiredFieldValidator style="color:red; font-size: 15px" runat="server" ControlToValidate="txtRazonSocial" ErrorMessage="*"></asp:RequiredFieldValidator>
     </div>
       <div class="mb-3">
-        <label for="CUIT" class="form-label">CUIT</label>       
-        <asp:TextBox runat="server" ID="txtCuit" class="form-control" />
+        <label for="txtCUIT" class="form-label">CUIT</label>       
+        <asp:TextBox runat="server" ID="txtCUIT" class="form-control" />
         <asp:RequiredFieldValidator style="color:red; font-size: 15px" runat="server" ControlToValidate="txtCuit" ErrorMessage="*"></asp:RequiredFieldValidator>
     </div>
      <div class="mb-3">
-        <label for="Domicilio" class="form-label">Domicilio</label>       
+        <label for="txtDomicilio" class="form-label">Domicilio</label>       
         <asp:TextBox runat="server" ID="txtDomicilio" class="form-control" />
         <asp:RequiredFieldValidator style="color:red; font-size: 15px" runat="server" ControlToValidate="txtDomicilio" ErrorMessage="*"></asp:RequiredFieldValidator>
     </div>
       <div class="mb-3">
-        <label for="Telefono" class="form-label">Telefono</label>       
+        <label for="txtTelefono" class="form-label">Telefono</label>       
         <asp:TextBox runat="server" ID="txtTelefono" class="form-control" />
         <asp:RequiredFieldValidator style="color:red; font-size: 15px" runat="server" ControlToValidate="txtTelefono" ErrorMessage="*"></asp:RequiredFieldValidator>
     </div>
       <div class="mb-3">
-        <label for="Email" class="form-label">Email</label>       
+        <label for="txtEmail" class="form-label">Email</label>       
         <asp:TextBox runat="server" ID="txtEmail" class="form-control" />
         <asp:RequiredFieldValidator style="color:red; font-size: 15px" runat="server" ControlToValidate="txtEmail" ErrorMessage="*"></asp:RequiredFieldValidator>
     </div>
 
    <br />
   
-    <asp:Button ID="btnAgregar" runat="server" Text="Agregar" OnClick="btnAgregar_Click" class="btn btn-success" />
-              
+    <asp:Button ID="btnAgregar" runat="server" Text="Agregar" OnClick="btnAgregar_Click" class="btn btn-success" visible="true"/>
+    <asp:Button ID="btnModificar" runat="server" Text="Modificar" OnClick="btnModificar_Click" CssClass="btn btn-success" visible="false"/>      
                       
     <script>
         //valida los campos solo numeros 
--- Formularios/Proveedores/ProveedoresABM.aspx.cs
+++ Formularios/Proveedores/ProveedoresABM.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,13 +15,57 @@ namespace Proyecto_Final_LAB.Formularios.Proveedores
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-             /*
-            if (Request.QueryString["id"] != null)
-            {             
-                int id = int.Parse(Request.QueryString["id"].ToString());                        
-            }  
-             */
+            ProveedorNegocio pn = new ProveedorNegocio();
+            try
+            {
+
+                if (Convert.ToInt32(Request.QueryString["accion"]) == 2 && !IsPostBack)
+                {
+                    int id = Convert.ToInt32(Request.QueryString["id"]);
+                    List<Proveedor> temp = (List<Proveedor>)Session["listaProveedores"];
+                    Proveedor selected = temp.Find(x => x.Id == id);
+                    btnAgregar.Visible = false;
+                    btnModificar.Visible = true;
+                    txtCodigo.Text = selected.Codigo;
+                    txtRazonSocial.Text = selected.RazonSocial;
+                    txtCUIT.Text = selected.Cuit;
+                    txtDomicilio.Text = selected.Domicilio;
+                    txtTelefono.Text = selected.Telefono;
+                    txtEmail.Text = selected.Email;                    
+                }
+                Session["listaProveedores"] = null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(Request.QueryString["id"]);
+                ProveedorNegocio pn = new ProveedorNegocio();
+                Proveedor p = new Proveedor();
+                p.Codigo = txtCodigo.Text;
+                p.RazonSocial = txtRazonSocial.Text;
+                p.Cuit = txtCUIT.Text;
+                p.Domicilio = txtDomicilio.Text;
+                p.Telefono = txtTelefono.Text;
+                p.Email = txtEmail.Text;              
+                if (pn.modificarProveedor(p, id))
+                {
+                    Session["alerta"] = "modificado";
+                    Response.Redirect("Proveedores.aspx", false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
 
         protected void btnAgregar_Click(object sender, EventArgs e)
         {
@@ -30,16 +75,24 @@ namespace Proyecto_Final_LAB.Formularios.Proveedores
 
                p.Codigo = txtCodigo.Text;
                p.RazonSocial = txtRazonSocial.Text;
-               p.Cuit = txtCuit.Text;
+               p.Cuit = txtCUIT.Text;
                p.Domicilio = txtDomicilio.Text;
                p.Telefono= txtTelefono.Text;
                p.Email= txtEmail.Text;
               
                if (pn.agregarProveedor(p))
                {
-                   ClientScript.RegisterClientScriptBlock(this.GetType(), "alert",
-                      "swal('Proveedor agregado', '', 'success')", true);
-               }           
+                Session["alerta"] = "agregado";
+                Response.Redirect("Proveedores.aspx");
+                // ClientScript.RegisterClientScriptBlock(this.GetType(), "alert",
+                //   "swal('Proveedor agregado', '', 'success')", true);
+            }           
         }
+
+
+
+
+
+
     }
 }
